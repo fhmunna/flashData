f92956f897fa08bc89923234b9963ad4878ee548
fhmunna <fhmunna@gmail.com>
2024-01-24 19:54:55 +0600

    added php.ini dev mode auto configuration script

---
+++ docker.sh
@@ -0,0 +1,17 @@
+#docker
+
+sudo apt install gnome-terminal
+
+sudo apt remove docker-desktop
+
+rm -r $HOME/.docker/desktop
+sudo rm /usr/local/bin/com.docker.cli
+sudo apt purge docker-desktop
+
+
+
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+
+
